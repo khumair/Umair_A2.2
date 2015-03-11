a8331bff2cc1520a1e3b934a75141743acc14770
Khawaja Umair <khawja.umair@gmail.com>
2015-03-11 18:07:35 +0200

    WindowsPhone 8.1
    Assigment of Developing interaction and design

---
+++ MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Storage; 
+using Windows.Storage.Streams; 
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI capture = new CameraCaptureUI();
+            capture.PhotoSettings.CroppedAspectRatio = new Size(16, 9);
+            StorageFile file = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            IRandomAccessStream filestream = await file.OpenAsync(FileAccessMode.Read);
+            BitmapImage img = new BitmapImage();
+            img.SetSource(filestream);
+            img_viewer.Source = img;
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI capture = new CameraCaptureUI();
+            capture.PhotoSettings.CroppedAspectRatio = new Size(16, 9);
+            StorageFile file = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            IRandomAccessStream filestream = await file.OpenAsync(FileAccessMode.Read);
+            BitmapImage img = new BitmapImage();
+            img.SetSource(filestream);
+            img_viewer1.Source = img;
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI capture = new CameraCaptureUI();
+            capture.PhotoSettings.CroppedAspectRatio = new Size(16, 9);
+            StorageFile file = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            IRandomAccessStream filestream = await file.OpenAsync(FileAccessMode.Read);
+            BitmapImage img = new BitmapImage();
+            img.SetSource(filestream);
+            img_viewer2.Source = img;
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI capture = new CameraCaptureUI();
+            capture.PhotoSettings.CroppedAspectRatio = new Size(16, 9);
+            StorageFile file = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            IRandomAccessStream filestream = await file.OpenAsync(FileAccessMode.Read);
+            BitmapImage img = new BitmapImage();
+            img.SetSource(filestream);
+            img_viewer3.Source = img;
+        }
+
+       
+
+    }
+}
+
+    
+
